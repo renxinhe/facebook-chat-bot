513d03f60a2933163853ab734a9eba8f60348b82
Jim Ren <jren@fitbit.com>
2016-07-27 10:52:26 -0700

    Fixed getHelp if spacing and console.long bug

--- public/javascript/getHelp.js
+++ public/javascript/getHelp.js
@@ -8,7 +8,7 @@ module.exports = function getHelp(api, threadID, body) {
     var worked = true;
     var go = true;
     var command = body.substring('@help'.length);
-    if(command.length==0) {
+    if (command.length==0) {
         api.sendMessage("Note: Add @ at the beginning of every command", threadID);
         api.sendMessage("Command list:\nhelp [command]\nweather [zip code]\nweather [city, state]\ncolor [#hex]\nmeme", threadID);
         go=false;
@@ -16,36 +16,36 @@ module.exports = function getHelp(api, threadID, body) {
     else {
         command=command.substring(1);
     }
-    if(go && command==='color') {
+    if (go && command==='color') {
         api.sendMessage("Usage of color:\nAfter the word color, type a # and then the hexadecimal value of a color\nThis command changes the chat colors to the color inputted", threadID);
         api.sendMessage("Examples:\ncolor #FFFF00\ncolor #a10dce", threadID);
         api.sendMessage("Note: Add @ at the beginning of every command", threadID);
     }
-    else if(go && command==='help') {
+    else if (go && command==='help') {
         api.sendMessage("Usage of help:\nReplace the second instance of the word help in your most recent request, with a different command to get more information about that command", threadID);
         api.sendMessage("Note: Add @ at the beginning of every command", threadID);
     }
-    else if(go && command==='meme') {
+    else if (go && command==='meme') {
         api.sendMessage("Usage of meme:\nUse meme to get rickrolled wonderfully", threadID);
         api.sendMessage("Note: Add @ at the beginning of every command", threadID);
     }
-    else if(go && command==='weather') {
+    else if (go && command==='weather') {
         api.sendMessage("Usage of weather:\nAfter the word weather, type either the zipcode or city, state of the place that you wish to get weather information about.\nYou will recieve current conditions as well as the 5-day forcast.", threadID);
         api.sendMessage("Examples:\nweather New York, NY\nweather Paris, France\nweather 75703", threadID);
         api.sendMessage("Note: Add @ at the beginning of every command", threadID);
     }
-    else if(go){
+    else if (go){
         api.sendMessage("Error: Unidentified command", threadID);
         worked = false;
     }
     //to expand upon expansion
-    if(command.length==0) {
+    if (command.length==0) {
         console.log("Help given in "+threadID);
     }
-    else if(worked) {
+    else if (worked) {
         console.log("Help given in "+threadID+" about "+command);
     }
     else {
-        console.long("Pfft... User entered help command wrong");
+        console.log("Pfft... User entered help command wrong");
     }
 }
