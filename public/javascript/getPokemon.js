4c7a59784ccbaf31e6cfe1aa58cdadf616ee1341
Jim Ren <jren@fitbit.com>
2016-07-28 16:49:22 -0700

    Finished static map logic without testing

--- public/javascript/getPokemon.js
+++ public/javascript/getPokemon.js
@@ -5,10 +5,15 @@
  */
  
 var request = require('request');
-var userAPI, userThreadID, endTyping;
+const fs = require('fs');
+var userAPI, userThreadID, endTyping, latitude, longitude;
 const POST_TOKEN_DELAY_MS = 3000;
 
-function getToken(latitude, longitude) {
+// Flags
+var showList = false;
+var mapPokemonID = [];
+
+function getToken() {
     var tokenRequest = {
         url: 'https://pokevision.com/map/scan/' + latitude + '/' + longitude,
         json: true
@@ -18,9 +23,10 @@ function getToken(latitude, longitude) {
         if (!error && res.statusCode == 200) {
             if (body.status == 'success') {
                 console.log('Token request success!');
-                setTimeout(function() {getPokemonList(latitude, longitude, body.jobId);}, POST_TOKEN_DELAY_MS);
+                setTimeout(function() {getPokemonList(body.jobId);}, POST_TOKEN_DELAY_MS);
                 return;
             } else {
+                userAPI.sendMessage('Cannot retreive API token. Please check www.pokevision.com for Pokemon Trainer Club status.', userThreadID);
                 console.error('Token request status: ' + body.status);
                 return console.error(body);
             }
@@ -28,7 +34,7 @@ function getToken(latitude, longitude) {
     });
  }
 
-function getPokemonList(latitude, longitude, token) {
+function getPokemonList(token) {
     var pokemonRequest = {
         url: 'https://pokevision.com/map/data/' + latitude + '/' + longitude +'/' + token,
         json: true
@@ -86,13 +92,40 @@ function parsePokemonList(pokemon) {
     }
     pokemonIDDict = sortOnKeys(pokemonIDDict);
     
-    var pokemonListString = 'Pokemon near by:\n';
+    var pokemonListString = 'Pokemon nearby:\n';
     for (var id in pokemonIDDict) {
         if (pokemonIDDict.hasOwnProperty(id)) {
-            pokemonListString += '#' + id + ' ' + pokemonNameDict[id] + ':\t\t\t' + pokemonIDDict[id].length + ' found\n';
+            pokemonListString += '#' + id + ' ' + 
+                                String(pokemonNameDict[id] + ':                    ').slice(0, 20) + 
+                                pokemonIDDict[id].length + ' found\n';
         }
     }
-    userAPI.sendMessage(pokemonListString, userThreadID);
+    if (showList) {
+        userAPI.sendMessage(pokemonListString, userThreadID);
+    } else {
+        // Constructing static map URL
+        var mapURL = 'https://maps.googleapis.com/maps/api/staticmap?';
+        mapURL += 'center=' + latitude + ',' + longitude + '&';
+        mapURL += 'scale=2&';
+        for (var i = 0; i < mapPokemonID.length; i++) {
+            mapURL += 'markers=icon:http://pkmn.net/sprites/frlg/' + mapPokemonID[i] + '.png%7C';
+            var pokemonListByID = pokemonIDDict[mapPokemonID[i]];
+            for (var j = 0; j < pokemonListByID.length; j++) {
+                mapURL += pokemonListByID[j].latitude + ',' + pokemonListByID[j].longitude + '%7C';
+            }
+        }
+
+        request
+            .get(mapURL)
+            .pipe(fs.createWriteStream('temp_pokemon.png'))
+            .on('close', function(response) {
+                var map = {
+                    // body: "Fuck Javascript!",
+                    attachment: fs.createReadStream('temp_pokemon.png')
+                };
+                userAPI.sendMessage(map, userThreadID);
+            });
+    }
     if (endTyping != null) {
         endTyping();
     }
@@ -115,18 +148,30 @@ function sortOnKeys(dict) {
     return tempDict;
 }
 
+function parseBody(body) {
+    if (body == '@pokemon') {
+        showList = true;
+        mapPokemonID = [];
+    } else {
+        body = body.substring('@pokemon '.length);
+        showList = false;
+        mapPokemonID = body.split(' ');
+    }
+}
+
 module.exports = function getPokemon(api, threadID, userID, body) {
     userAPI = api;
     userThreadID = threadID;
+    parseBody(body);
+    console.log('threadID' + threadID);
 
-    var latitude, longitude;
     var Location = require('./model.js').Location;
     Location.where({userID: String(userID)}).findOne(function(err, location) {
         if (err) {
             return console.error(err);
         }
         if (location == null) {
-            api.sendMessage('Location data not found. Please first specify a location via messenger location sharing.', threadID);
+            api.sendMessage('User location data not found. Please first send a location via Messenger location sharing.', threadID);
             return console.log('User "' + userID + '" location info not found in database.');
         } else {
             latitude = location['latitude'];
@@ -143,4 +188,4 @@ module.exports = function getPokemon(api, threadID, userID, body) {
 
 };
 
-var pokemonNameDict = {1: 'bulbasaur', 2: 'ivysaur', 3: 'venusaur', 4: 'charmander', 5: 'charmeleon', 6: 'charizard', 7: 'squirtle', 8: 'wartortle', 9: 'blastoise', 10: 'caterpie', 11: 'metapod', 12: 'butterfree', 13: 'weedle', 14: 'kakuna', 15: 'beedrill', 16: 'pidgey', 17: 'pidgeotto', 18: 'pidgeot', 19: 'rattata', 20: 'raticate', 21: 'spearow', 22: 'fearow', 23: 'ekans', 24: 'arbok', 25: 'pikach', 26: 'raich', 27: 'sandshrew', 28: 'sandslash', 29: 'nidoran-f', 30: 'nidorina', 31: 'nidoqueen', 32: 'nidoran-m', 33: 'nidorino', 34: 'nidoking', 35: 'clefairy', 36: 'clefable', 37: 'vulpix', 38: 'ninetales', 39: 'jigglypuff', 40: 'wigglytuff', 41: 'zubat', 42: 'golbat', 43: 'oddish', 44: 'gloom', 45: 'vileplume', 46: 'paras', 47: 'parasect', 48: 'venonat', 49: 'venomoth', 50: 'diglett', 51: 'dugtrio', 52: 'meowth', 53: 'persian', 54: 'psyduck', 55: 'golduck', 56: 'mankey', 57: 'primeape', 58: 'growlithe', 59: 'arcanine', 60: 'poliwag', 61: 'poliwhirl', 62: 'poliwrath', 63: 'abra', 64: 'kadabra', 65: 'alakazam', 66: 'machop', 67: 'machoke', 68: 'machamp', 69: 'bellsprout', 70: 'weepinbell', 71: 'victreebel', 72: 'tentacool', 73: 'tentacruel', 74: 'geodude', 75: 'graveler', 76: 'golem', 77: 'ponyta', 78: 'rapidash', 79: 'slowpoke', 80: 'slowbro', 81: 'magnemite', 82: 'magneton', 83: 'farfetchd', 84: 'doduo', 85: 'dodrio', 86: 'seel', 87: 'dewgong', 88: 'grimer', 89: 'muk', 90: 'shellder', 91: 'cloyster', 92: 'gastly', 93: 'haunter', 94: 'gengar', 95: 'onix', 96: 'drowzee', 97: 'hypno', 98: 'krabby', 99: 'kingler', 100: 'voltorb', 101: 'electrode', 102: 'exeggcute', 103: 'exeggutor', 104: 'cubone', 105: 'marowak', 106: 'hitmonlee', 107: 'hitmonchan', 108: 'lickitung', 109: 'koffing', 110: 'weezing', 111: 'rhyhorn', 112: 'rhydon', 113: 'chansey', 114: 'tangela', 115: 'kangaskhan', 116: 'horsea', 117: 'seadra', 118: 'goldeen', 119: 'seaking', 120: 'stary', 121: 'starmie', 122: 'mr-mime', 123: 'scyther', 124: 'jynx', 125: 'electabuzz', 126: 'magmar', 127: 'pinsir', 128: 'tauros', 129: 'magikarp', 130: 'gyarados', 131: 'lapras', 132: 'ditto', 133: 'eevee', 134: 'vaporeon', 135: 'jolteon', 136: 'flareon', 137: 'porygon', 138: 'omanyte', 139: 'omastar', 140: 'kabuto', 141: 'kabutops', 142: 'aerodactyl', 143: 'snorlax', 144: 'articuno', 145: 'zapdos', 146: 'moltres', 147: 'dratini', 148: 'dragonair', 149: 'dragonite', 150: 'mewtwo', 151: 'mew'};
+var pokemonNameDict = {1: 'Bulbasaur', 2: 'Ivysaur', 3: 'Venusaur', 4: 'Charmander', 5: 'Charmeleon', 6: 'Charizard', 7: 'Squirtle', 8: 'Wartortle', 9: 'Blastoise', 10: 'Caterpie', 11: 'Metapod', 12: 'Butterfree', 13: 'Weedle', 14: 'Kakuna', 15: 'Beedrill', 16: 'Pidgey', 17: 'Pidgeotto', 18: 'Pidgeot', 19: 'Rattata', 20: 'Raticate', 21: 'Spearow', 22: 'Fearow', 23: 'Ekans', 24: 'Arbok', 25: 'Pikachu', 26: 'Raichu', 27: 'Sandshrew', 28: 'Sandslash', 29: 'Nidoran♀', 30: 'Nidorina', 31: 'Nidoqueen', 32: 'Nidoran♂', 33: 'Nidorino', 34: 'Nidoking', 35: 'Clefairy', 36: 'Clefable', 37: 'Vulpix', 38: 'Ninetales', 39: 'Jigglypuff', 40: 'Wigglytuff', 41: 'Zubat', 42: 'Golbat', 43: 'Oddish', 44: 'Gloom', 45: 'Vileplume', 46: 'Paras', 47: 'Parasect', 48: 'Venonat', 49: 'Venomoth', 50: 'Diglett', 51: 'Dugtrio', 52: 'Meowth', 53: 'Persian', 54: 'Psyduck', 55: 'Golduck', 56: 'Mankey', 57: 'Primeape', 58: 'Growlithe', 59: 'Arcanine', 60: 'Poliwag', 61: 'Poliwhirl', 62: 'Poliwrath', 63: 'Abra', 64: 'Kadabra', 65: 'Alakazam', 66: 'Machop', 67: 'Machoke', 68: 'Machamp', 69: 'Bellsprout', 70: 'Weepinbell', 71: 'Victreebel', 72: 'Tentacool', 73: 'Tentacruel', 74: 'Geodude', 75: 'Graveler', 76: 'Golem', 77: 'Ponyta', 78: 'Rapidash', 79: 'Slowpoke', 80: 'Slowbro', 81: 'Magnemite', 82: 'Magneton', 83: 'Farfetchd', 84: 'Doduo', 85: 'Dodrio', 86: 'Seel', 87: 'Dewgong', 88: 'Grimer', 89: 'Muk', 90: 'Shellder', 91: 'Cloyster', 92: 'Gastly', 93: 'Haunter', 94: 'Gengar', 95: 'Onix', 96: 'Drowzee', 97: 'Hypno', 98: 'Krabby', 99: 'Kingler', 100: 'Voltorb', 101: 'Electrode', 102: 'Exeggcute', 103: 'Exeggutor', 104: 'Cubone', 105: 'Marowak', 106: 'Hitmonlee', 107: 'Hitmonchan', 108: 'Lickitung', 109: 'Koffing', 110: 'Weezing', 111: 'Rhyhorn', 112: 'Rhydon', 113: 'Chansey', 114: 'Tangela', 115: 'Kangaskhan', 116: 'Horsea', 117: 'Seadra', 118: 'Goldeen', 119: 'Seaking', 120: 'Staryu', 121: 'Starmie', 122: 'Mr-Mime', 123: 'Scyther', 124: 'Jynx', 125: 'Electabuzz', 126: 'Magmar', 127: 'Pinsir', 128: 'Tauros', 129: 'Magikarp', 130: 'Gyarados', 131: 'Lapras', 132: 'Ditto', 133: 'Eevee', 134: 'Vaporeon', 135: 'Jolteon', 136: 'Flareon', 137: 'Porygon', 138: 'Omanyte', 139: 'Omastar', 140: 'Kabuto', 141: 'Kabutops', 142: 'Aerodactyl', 143: 'Snorlax', 144: 'Articuno', 145: 'Zapdos', 146: 'Moltres', 147: 'Dratini', 148: 'Dragonair', 149: 'Dragonite', 150: 'Mewtwo', 151: 'Mew'};
