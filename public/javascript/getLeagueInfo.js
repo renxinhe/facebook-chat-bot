5415a1075445d9ed940a39ec3494cd45e9cdad1b
Jim Ren <jren@fitbit.com>
2016-08-05 14:36:27 -0700

    Added find match info feature through LoLNexus

--- public/javascript/getLeagueInfo.js
+++ public/javascript/getLeagueInfo.js
@@ -7,8 +7,9 @@
  
 var request = require('request');
 var MetaInspector = require('node-metainspector');
+var cheerio = require('cheerio');
 
-var userAPI, userThreadID, endTyping;
+var userAPI, userThreadID, endTyping, returnString;
 
 function setName(userID, name) {
     var Summoner = require('./model.js').Summoner;
@@ -33,15 +34,12 @@ function getSummonerInfo(name) {
         var info = client.description;
         if (info == 'LoL Stats! Check your Summoner, MMR, Live Spectate and using powerful global League of Legends Statistics!') {
             userAPI.sendMessage('Summoner "' + name + '" not found.', userThreadID);
+            return;
         } else {
             info = info.replace(/( \/ )|(, )/g, '\n');
-            userAPI.sendMessage(info, userThreadID);
+            returnString += info + '\n----------------\n';
+            getMatchInfo(name);
         }
-
-        if (endTyping != undefined) {
-            endTyping();
-        }
-        return;
     });
      
     client.on("error", function(err){
@@ -51,9 +49,81 @@ function getSummonerInfo(name) {
     client.fetch();
 }
 
+function getMatchInfo(name) {
+    var options = {
+        url: 'http://www.lolnexus.com/ajax/get-game-info/NA.json?name=' + name,
+        json: true
+    }
+    console.log('Requesting lolnexus...');
+    userAPI.sendMessage('Requesting info from LoLNexus. Please wait...', userThreadID);
+    request.get(options, function(err, res, body) {
+        if (err) {
+            console.error('Failed to get match info.');
+            console.error(err);
+            return;
+        }
+        if ((/.+is not currently in a game.+/).test(body['html'])) {
+            console.log(name + ' is not currently in a game.');
+            returnString += name + ' is not currently in a game.\n';
+            printResponse(returnString);
+            return;
+        } else if (!body['successful']) {
+            console.log('Game lookup unsuccessful.');
+            returnString += 'Game lookup unsuccessful for ' + name + '.\n';
+            printResponse(returnString);
+            return;
+        }
+        console.log('html loaded.');
+        var html = body['html'].replace(/\r\n/g, '');
+        html = body['html'].replace(/\\"/g, '"');
+        html = body['html'].replace(/\r\n/g, '');
+
+        var $ = cheerio.load(html);
+        var team1 = [];
+        $('tbody > tr', 'div.team-1').each(function(i, element) {
+            team1[i] = $(this).children('td.name').children('a').children('span').text();
+            team1[i] += ' - ' + $(this).children('td.champion').children('span').children('a').text();
+            if ($(this).children('td.level').text() != '') {
+                team1[i] += ' - Level: ' + $(this).children('td.level').text();
+            }
+            team1[i] += ' - ' + $(this).children('td.current-season').children('div.ranking').children('span').first().text();
+        });
+        var team2 = [];
+        $('tbody > tr', 'div.team-2').each(function(i, element) {
+            team2[i] = $(this).children('td.name').children('a').children('span').text();
+            team2[i] += ' - ' + $(this).children('td.champion').children('span').children('a').text();
+            if ($(this).children('td.level').text() != '') {
+                team2[i] += ' - Level: ' + $(this).children('td.level').text();
+            }
+            team2[i] += ' - ' + $(this).children('td.current-season').children('div.ranking').children('span').first().text();
+        });
+        // console.log($('.team-1').children('table > tbody > tr > .name > a > span'));
+        var matchInfo = 'Current Match:\nTeam 1\n';
+        for (var i = 0; i < team1.length ; i++) {
+            matchInfo += team1[i] + "\n";
+        }
+        matchInfo += '\nTeam 2\n';
+        for (var i = 0; i < team2.length ; i++) {
+            matchInfo += team2[i] + "\n";
+        }
+        returnString += matchInfo;
+        printResponse(returnString);
+
+        if (endTyping != undefined) {
+            endTyping();
+        }
+        return;
+    })
+}
+
+function printResponse(res) {
+    userAPI.sendMessage(res, userThreadID);
+}
+
 module.exports = function getLeagueInfo(api, threadID, userID, body) {
     userAPI = api;
     userThreadID = threadID;
+    returnString = '';
 
     if (body == '@league') {
         var Summoner = require('./model.js').Summoner;
